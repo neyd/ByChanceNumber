0a5c2b3f86c48542f49425abef5003f4d405f4e2
neyd <jev.neydel@gmail.com>
2015-10-06 21:23:17 +0300

    Crlt + Alt + L

--- src/org/neyd/ByChanceNumber/ByChanceNumber.java
+++ src/org/neyd/ByChanceNumber/ByChanceNumber.java
@@ -15,22 +15,20 @@ public class ByChanceNumber {
         double randomNumber;
         System.out.println("Загадайте число від 1 до 100 і натисніть будь-яку клавішу");
         resp = Integer.parseInt(scanner.nextLine());
-        randomNumberDown=1;
-        randomNumberTop=101;
-        while (resp!=2){
-            randomNumber = Math.floor((randomNumberDown+randomNumberTop)/2);
+        randomNumberDown = 1;
+        randomNumberTop = 101;
+        while (resp != 2) {
+            randomNumber = Math.floor((randomNumberDown + randomNumberTop) / 2);
             //число randomNumber мусить бути типу double тому, що воно інакше видає помилку
             //math.floor мусить бути, бо якщо (34+37)/2= 35,5
-            System.out.println("Ваше число" + randomNumber + "?");
+            System.out.println("Ваше число" + " " + randomNumber + "?");
             resp = Integer.parseInt(scanner.nextLine());
-            if(resp==2) {
+            if (resp == 2) {
                 System.out.println("Вітаємо");
-            }
-            else if(resp==1) {
+            } else if (resp == 1) {
                 randomNumberDown = randomNumber;
                 //число randomNumberDown теж має бути типу double бо інакше ми не зможемо присвоїти йому число типу double
-            }
-            else if (resp==0) {
+            } else if (resp == 0) {
                 randomNumberTop = randomNumber;
                 //тоже саме
             }
